42b1576e7ba708461add5f120ab2740676917d90
Jan <jankoerner.1998@gmail.com>
2018-01-12 12:50:07 +0100

    Merge

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,22 +4,28 @@ import com.aldebaran.qi.Application;
 import com.aldebaran.qi.helper.proxies.ALAnimatedSpeech;
 import com.aldebaran.qi.helper.proxies.ALMotion;
 import com.aldebaran.qi.helper.proxies.ALRobotPosture;
+import com.aldebaran.qi.helper.proxies.ALAnimatedSpeech;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.TextFlow;
 
 public class Controller {
-    @FXML TextField tx_IP;
-    @FXML TextField tx_Port;
+    @FXML TextField tx_IP, tx_Port;
     @FXML Slider velocitySlider;
+    @FXML TextFlow tfl_log;
     @FXML TextArea textToSpeech;
+    @FXML Button w,a,s,d, connectButton, disconnectButton;
+    @FXML Circle connectCircle;
+
     private Application app;
+    private ConnectionModel connectionModel;
     private MovementModel movementModel = new MovementModel();
     private ALMotion alMotion;
     private Boolean first = true;
-    private ConnectionModel connectionModel;
     private TextToSpeechModel textToSpeechModel;
     private MoveHeadModel moveHeadModel = new MoveHeadModel();
 
@@ -27,28 +33,38 @@ public class Controller {
     Logger logger = new Logger(log, "");
 
     public static void main(String[] args) {
-        //TODO vielleicht hier Eingabe einer URL forcen
 
     }
 
     public void btn_ConnectIsPressed(ActionEvent actionEvent) throws Exception {
-         connectionModel = new ConnectionModel();
-        if (connectionModel.connect(tx_IP.getText(), Integer.parseInt(tx_Port.getText())))
-        {
-            if(app == null){
-                app = new Application(new String[] {},connectionModel.getNaoUrl());
-            }
-            app.start();
-            ALAnimatedSpeech alAnimatedSpeech = new ALAnimatedSpeech(app.session());
-            alAnimatedSpeech.say("You are connected");
-
+        if (connectionModel == null){
+            connectionModel = new ConnectionModel();
         }
-        else{
-            logger.warn("IP stimmt nicht oder Port stimmt nicht, bitte Verbindung überprüfen");
+        if(tx_Port.getText() != null && tx_IP.getText() != null){
+            if (connectionModel.connect(tx_IP.getText(), Integer.parseInt(tx_Port.getText())))
+            {
 
+                if(app == null){
+                    app = new Application(new String[] {},connectionModel.getNaoUrl());
+                    app.start();
+                }
+
+                if (app.session().isConnected()){
+                    connectCircle.setFill(Color.rgb(60,230,30));
+                    connectButton.setDisable(true);
+                    disconnectButton.setDisable(false);
+                    ALAnimatedSpeech alAnimatedSpeech = new ALAnimatedSpeech(app.session());
+                    alAnimatedSpeech.say("You are connected");
+                }
+
+            }
+            else{
+                logger.warn("IP stimmt nicht oder Port stimmt nicht, bitte Verbindung überprüfen");
+            }
         }
 
     }
+
     public void moveKeyBoard(KeyEvent keyEvent)throws Exception{
         if (app != null){
             if (first){
@@ -65,8 +81,6 @@ public class Controller {
                 posture.goToPosture("Stand", 1.0f);
             }
         }
-
-
     }
 
     public void move(ActionEvent actionEvent)throws Exception{
@@ -75,8 +89,11 @@ public class Controller {
     }
 
     public void say(ActionEvent actionEvent)throws Exception{
-        if (textToSpeech.getText() != null){
+        if (textToSpeechModel == null)
+        {
             textToSpeechModel = new TextToSpeechModel();
+        }
+        if (textToSpeech.getText() != null){
             textToSpeechModel.say(app, textToSpeech.getText());
         }
     }
@@ -94,5 +111,11 @@ public class Controller {
             ALRobotPosture posture = new ALRobotPosture(app.session());
             posture.goToPosture("Crouch", 1.0f);
         }
+    }
+
+    public void disconnect(){
+        connectCircle.setFill(Color.rgb(240,20,20));
+        connectButton.setDisable(false);
+        disconnectButton.setDisable(true);
     }
 }
